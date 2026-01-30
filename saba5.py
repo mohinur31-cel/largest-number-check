20fe9d41406387204a896da5069dc45dc75cfb99
mohinur31-cel <rahmanmohinur897@gmail.com>
2026-01-30 16:59:51 +0600

    Add files via upload

---
+++ saba5.py
@@ -0,0 +1,10 @@
+a = int(input("Enter First number : "))
+b =int(input("Enter second number : "))
+C =int(input("Enter third number : "))
+if (a>=b and a>=C ):
+    print("a is the largest number",a)
+elif (b>=a):
+    print("b is the largest number",b)
+else:
+        print("C is the largest number",C)
+       
